a36f625ba31c8933612fc9a5d3341ea6761d7b96
Макеева Татьяна <tyh94@mail.ru>
2026-06-27 16:57:44 +0700

    Update storage

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.3"),
+        .package(url: "https://github.com/tyh94/Storage.git", from: "1.0.4"),
         .package(url: "https://github.com/tyh94/MKVNetwork.git", from: "1.0.0"),
         
         .package(url: "https://github.com/yandexmobile/yandex-login-sdk-ios.git", from: "3.1.0"),
